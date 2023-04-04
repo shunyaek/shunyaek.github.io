9721040a3f4876280fa9811a89af2db65b61a62c
Harshit Doshi <5103046+HarshitDoshi@users.noreply.github.com>
2023-04-04 15:54:36 +0530

    new

--- client/app/header.tsx
+++ client/app/header.tsx
@@ -1,16 +1,23 @@
+type TabType = {
+  title: string;
+  path: string;
+}
+
+
 export default function Header() {
-  const tabs = [
+  const tabs: TabType[] = [
+    { title: "about", path: "#" },
+    { title: "work", path: "#" },
     { title: "blog", path: "#" },
-    { title: "projects", path: "#" },
-    { title: "contact", path: "#" },
   ];
   return (
     <header>
       <nav role="navigation">
-        {/* <a href="/">Harshit Doshi</a> */}
         {
-          tabs.map((tab: { title: string; path: string }) => {
-            return <a key={tab.path} href={tab.path}>{tab.title}</a>;
+          tabs.map((tab: TabType) => {
+            return (<a key={tab.path} href={tab.path}>
+              {tab.title}
+            </a>);
           })
         }
       </nav>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,17 +1,52 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from "./page.module.css"
+import Header from './header'
+import Footer from './footer'
+
+const TechnologyPill = ({ technology }: { technology: string; }) => {
+  return (
+    <div className={styles.technology}>#{technology}</div>
+  );
+}
 
 export default function Home() {
+  const technologies = [
+    "Python", "JavaScript", "TypeScript",
+    "Django", "Flask", "FastAPI", "NodeJS",
+    "ReactJS", "NextJS", "ReduxJS", "React_Query",
+    "React_Native", "Flutter",
+    "TailwindCSS", "Styled_Components", "Material-UI",
+    "REST", "GraphQL",
+    "Authentication", "Authorisation", "SSO",
+    "Git", "GitHub",
+    "Cloudflare", "Vercel", "AWS", "Google_Cloud",
+    "Docker", "Kubernetes",
+    "PostgreSQL", "MongoDB", "Redis",
+    "StorybookJS", "Jupyter_Notebooks",
+  ]
   return (
-    <section className={styles.logo}>
-      <Image
-        src="/SE_Logo.svg"
-        alt="shunyaek Logo"
-        width={540}
-        height={200}
-        priority
-      />
-    </section>
+    <>
+      <article className={styles.sidepane}>
+        <Header />
+        <section className={styles.introductionsection}>
+          <div>
+            <span className={styles.introductionquotes}>&quot;shunyaek&quot;</span> is a combination of two words: <span className={styles.introductionquotes}>&quot;shunya&quot;</span> & <span className={styles.introductionquotes}>&quot;ek&quot;</span>. They mean <span className={styles.introductionquotes}>&quot;zero&quot;</span> and <span className={styles.introductionquotes}>&quot;one&quot;</span> in Hindi respectively. In the world of computers & technology, these binary digits or bits are the building blocks of everything. At shunyaek, we create magic from these bits for you!
+          </div>
+        </section>
+        <section>
+          <div className={styles.technologytitle}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+            </svg>
+            <div>Technology</div>
+          </div>
+          <div className={styles.technologies}>
+            {technologies.map((technology) => {
+              return <TechnologyPill key={technology} technology={technology} />;
+            })}
+          </div>
+        </section>
+        <Footer />
+      </article>
+    </>
   )
 }
