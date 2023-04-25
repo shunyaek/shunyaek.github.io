462de3d1df1761ab09a632007c5db23498b0342e
Harshit Doshi <5103046+HarshitDoshi@users.noreply.github.com>
2023-04-25 18:31:32 +0530

    fix favicon

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="./public/favicon.ico" sizes="any" />
+        <link rel="icon" href="./public/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#4a4a4a" />
