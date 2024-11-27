2dea1aec291a224a31dea8aa0ab8063ea35e035e
HarshitDoshi <5103046+HarshitDoshi@users.noreply.github.com>
2024-11-27 19:51:17 +0530

    remove unused files and configurations from the client directory

--- client/app/harshit/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "../page.module.css"
-
-export default function Harshit() {
-  return (
-    <article className={styles.sidepane}>
-      {"$\t"}<input className={styles.stdin} type={"text"} />
-    </article>
-  )
-}
